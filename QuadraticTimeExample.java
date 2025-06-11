fdc3ddbdee0adba29fc995e8042361de0d559c51
Akshai Sai <akshaisai21@gmail.com>
2025-06-11 12:06:42 +0530

    Add files via upload

---
+++ QuadraticTimeExample.java
@@ -0,0 +1,20 @@
+package demo;
+
+public class QuadraticTimeExample {
+	public static void main(String[] args) {
+		int[] numbers = {1, 3, 5, 7, 9, 11};
+		boolean hasDuplicate = false;
+		for(int i=0;i<numbers.length;i++) {
+			for(int j=i+1;j<numbers.length;j++) {
+				if(numbers[i] == numbers[j]) {
+					hasDuplicate = true;
+					System.out.println("Duplicate found:" + numbers[i]);
+				}
+			}
+		}
+		if(!hasDuplicate) {
+			System.out.println("No duplicate found.");
+		}
+	}
+
+}
